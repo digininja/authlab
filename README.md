a5902a6f49cce42397ead684affc7aed9ae1a7bf
Robin <robin@digi.ninja>
2019-05-09 16:08:37 +0100

    Ready for launch

---
+++ README.md
@@ -0,0 +1,43 @@
+# Authentication Lab
+
+Welcome to the Authentication Lab. The lab is a selection of challenges all related to authentication or authorisation. They are all taken, in some way, from real world examples I've come across during tests or have been suggested by other testers.
+
+You can play the latest version of all the challenges in my <a href="https://authlab.digi.ninja">online version</a> or grab the code from here build your own lab. The lab is written in Golang and should be fairly easy to install and get running.
+
+For more information, see my <a href="https://digi.ninja/projects/authlab.php">project page</a>. This also contains more information and walkthroughs for all the labs in case you get stuck.
+
+## Installation
+
+First you will need Go setup on your machine, I'll leave that up to you to work through as there are plenty of resources out there to help with that.
+
+Once you have a working version, you can get the lab by running:
+
+```
+go get github.com/digininja/authlab
+```
+
+This will download the project into your go source directory, on a Linux box, this will probably be `~/go/src/github.com/digininja/authlab`. Change into that directory and run:
+
+```
+go get -d ./...
+```
+
+This will download and install all the dependencies.
+
+The application uses the Revel framework to handle all the web stuff. At the moment, there is a bug which prevents it from working correctly the first time it is started up, to get round this, you need to run the binary as follows to set everything up correctly:
+
+```
+~/go/bin/revel version
+```
+
+Assuming this runs OK, and does not give any errors, you are good to go.
+
+## Starting the lab
+
+You can start the lab with the following command:
+
+```
+~/go/bin/revel run -a github.com/digininja/authlab/
+```
+
+Then browse to <http://localhost:9000>
---
+++ app/controllers/app.go
@@ -0,0 +1,242 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/revel/revel"
+	"strings"
+	"time"
+)
+
+// For HMAC signing method, the key can be any []byte. It is recommended to generate
+// a key using crypto/rand or something equivalent. You need the same key for signing
+// and validating.
+var hmacSampleSecret []byte
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+/*
+
+Creds are:
+
+sid / ThisIsLongSecurePassword
+
+*/
+
+func (c App) ClientSide(hash string) revel.Result {
+	logged_in := false
+	if hash == "e2b18481be9c7b210e3fa881d7484495" {
+		logged_in = true
+	}
+
+	return c.Render(logged_in)
+}
+
+func (c App) Timing() revel.Result {
+	return c.Render()
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func (c App) Timing_Login(username, password string) revel.Result {
+	users := make([]string, 4)
+	users[0] = "zoe"
+	users[1] = "joe"
+	users[2] = "alex"
+	users[3] = "sarah"
+
+	if contains(users, strings.ToLower(username)) {
+		time.Sleep(3000 * time.Millisecond)
+
+	}
+	c.Flash.Error("Login Failed")
+	c.FlashParams()
+	return c.Redirect(App.Timing)
+}
+
+/*
+
+Auth0 vulnerability
+
+https://auth0.com/docs/security/bulletins/cve-2019-7644
+
+https://godoc.org/github.com/dgrijalva/jwt-go#example-Parse--ErrorChecking
+
+*/
+
+func getToken(token *jwt.Token) (interface{}, error) {
+	// Don't forget to validate the alg is what you expect:
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	}
+
+	// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+	hmacSampleSecret = []byte("my_secret_key")
+	return hmacSampleSecret, nil
+}
+
+func ParseJWT(tokenString string) (bool, string) {
+	var success bool = false
+	var message string = ""
+
+	token, err := jwt.Parse(tokenString, getToken)
+
+	if err == nil {
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			message = fmt.Sprintf("Welcome %s (%s)", claims["user"], claims["level"])
+			success = true
+		}
+	} else if ve, ok := err.(*jwt.ValidationError); ok {
+		// This is from <https://godoc.org/github.com/dgrijalva/jwt-go#pkg-constants>
+		if ve.Errors&jwt.ValidationErrorSignatureInvalid != 0 {
+			newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, token.Claims)
+			newTokenString, _ := newToken.SignedString(hmacSampleSecret)
+
+			newParsedToken, _ := jwt.Parse(newTokenString, getToken)
+
+			message = fmt.Sprintf("Invalid signature. Expected %s got %s", newParsedToken.Signature, token.Signature)
+			//message = fmt.Sprintf("err: %s", err)
+		} else if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+			message = fmt.Sprintln("That's not even a token")
+		} else {
+			fmt.Sprintln("Couldn't handle this token:", err)
+		}
+	} else {
+		message = fmt.Sprintf("There was an error parsing the token: %s", err.Error())
+	}
+
+	return success, message
+}
+
+func (c App) Auth1_Login(jwt string) revel.Result {
+	success, message := ParseJWT(jwt)
+
+	if success {
+		c.Flash.Success(message)
+	} else {
+		c.Flash.Error(message)
+	}
+	c.FlashParams()
+
+	return c.Redirect(App.Auth1)
+}
+func (c App) Auth1() revel.Result {
+	/*
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": "joe",
+			"password": "2ac9cb7dc02b3c0083eb70898e549b63",
+			"level":    "admin",
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, _ := token.SignedString(hmacSampleSecret)
+	*/
+
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZXZlbCI6InVzZXIiLCJ1c2VyIjoic2lkIn0.Hnpn5k6NtrXn8qvOuiSsFjXhAolQGn3TfmGBvA7EGTU"
+
+	robin := "Robin"
+	//var username string = c.Params.Form["username"][0]
+	username := ""
+	return c.Render(tokenString, username, robin)
+}
+
+func (c App) Bypass() revel.Result {
+	forwarded := c.Request.Header.Get("X-Forwarded-For")
+	logged_in := false
+	if strings.Contains(forwarded, "192.168.0.14") {
+		logged_in = true
+	}
+
+	return c.Render(forwarded, logged_in)
+}
+
+/******************
+
+Expired JWT Lab
+
+******************/
+
+func (c App) Expired_JWT_Login(username, password string) revel.Result {
+	if username == "joe" && password == "Password1" {
+		c.Flash.Success("Login Success")
+	} else {
+		c.Flash.Error("Login Failed")
+	}
+	c.FlashParams()
+
+	return c.Redirect(App.Expired_JWT)
+}
+func (c App) Expired_JWT() revel.Result {
+	/*
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": "joe",
+			"password": "2ac9cb7dc02b3c0083eb70898e549b63",
+			"level":    "admin",
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, _ := token.SignedString(hmacSampleSecret)
+	*/
+
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZXZlbCI6ImFkbWluIiwicGFzc3dvcmQiOiIyYWM5Y2I3ZGMwMmIzYzAwODNlYjcwODk4ZTU0OWI2MyIsInVzZXJuYW1lIjoiam9lIn0.6j3NrK-0C7K8gmaWeB9CCyZuQKfvVEAl4KhitRN2p5k"
+
+	robin := "Robin"
+	//var username string = c.Params.Form["username"][0]
+	username := ""
+	return c.Render(tokenString, username, robin)
+}
+
+/******************
+
+Leaky JWT Lab
+
+******************/
+
+func (c App) Leaky_JWT_Login(username, password string) revel.Result {
+	if username == "joe" && password == "Password1" {
+		c.Flash.Success("Login Success")
+	} else {
+		c.Flash.Error("Login Failed")
+	}
+	c.FlashParams()
+
+	return c.Redirect(App.Leaky_JWT)
+}
+func (c App) Leaky_JWT() revel.Result {
+	/*
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": "joe",
+			"password": "2ac9cb7dc02b3c0083eb70898e549b63",
+			"level":    "admin",
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, _ := token.SignedString(hmacSampleSecret)
+	*/
+
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZXZlbCI6ImFkbWluIiwicGFzc3dvcmQiOiIyYWM5Y2I3ZGMwMmIzYzAwODNlYjcwODk4ZTU0OWI2MyIsInVzZXJuYW1lIjoiam9lIn0.6j3NrK-0C7K8gmaWeB9CCyZuQKfvVEAl4KhitRN2p5k"
+
+	robin := "Robin"
+	//var username string = c.Params.Form["username"][0]
+	username := ""
+	return c.Render(tokenString, username, robin)
+}
